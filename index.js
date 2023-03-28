fb2a0b18c59c949c40879f61e4cbf0695d706140
Joaquin Parada <Capacitaciones8@adviters.com>
2023-03-28 14:12:34 -0300

    primer commit

---
+++ index.js
@@ -0,0 +1,59 @@
+
+let url = "https://bootcamp-adviters.herokuapp.com";
+// let token = 1232323;
+
+
+const form = document.querySelector('form');
+
+form.addEventListener('submit', function (event) {
+    event.preventDefault();
+
+    let email = document.getElementById('idEmail').value;
+    let password = document.getElementById('idPassword').value;
+
+    // Realiza la autenticación aquí, por ejemplo, verificando que las credenciales sean correctas.
+    fetch(url + "/login",
+        {
+            method: "POST",
+            headers: { "content-type": "application/json" },
+            body: JSON.stringify({
+
+                "email": email,
+                "password": password
+
+            })
+        })
+        .then(res => res.json())
+        .then(data => {
+            let token = data.accessToken.stsTokenManager.accessToken;
+            localStorage.setItem('localToken', token);
+            getGrupo(token)
+
+        }
+        )
+        .catch(err => console.log(err))
+});
+
+
+
+
+
+function getGrupo(token) {
+
+    fetch(url + "/grupos",
+        {
+            headers: {
+                "content-type": "application/json",
+                Authorization: `bearer ${token}`
+            }
+        }
+    )
+
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+            localStorage.setItem('grupos', data);
+        })
+        .catch(err => console.error(err));
+
+}
